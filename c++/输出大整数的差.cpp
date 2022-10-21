b1550dba729b12997a4de847af00755a20dd6327
suoaaa <1062173732@qq.com>
2022-10-21 03:55:05 +0000

    123

---
+++ c++/输出大整数的差.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+void(char *a,char *b);
+int main()
+{
+    char a[15];
+    char b[15];
+    int i=0;
+    while(i<15)
+    {
+        cin>>a[i];
+        if(a[i]=='\n'||a[i]==' ')
+        {
+            break;
+        }
+    }
+    while(i<15)
+    {
+        cin>>b[i]
+        {
+            if(b[i]=='n'||b[i]==' ')
+            {
+                break;
+            }
+        }
+    }
+    void (a,b);
+    return 0;
+}
+void (char *a,char *b)
+{
+    int m=strlen(a);
+    int n=strlen(b);
+    int tem=0;//储存减位数字
+    char out[15];
+    for(;a>-1&&b>-1)
+}
