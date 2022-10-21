18cc4fc24f22f93ca3f34f4ef2d743f3632ec6f9
suoaaa <1062173732@qq.com>
2022-10-21 20:08:35 +0800

    late

--- c++/输出大整数的差.cpp
+++ c++/输出大整数的差.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
-void(char *a,char *b);
+void cal(char *a,char *b);
 int main()
 {
     char a[15];
@@ -9,30 +9,37 @@ int main()
     int i=0;
     while(i<15)
     {
-        cin>>a[i];
+        scanf("%c",&a[i]);
         if(a[i]=='\n'||a[i]==' ')
         {
             break;
         }
+        cout<<a[i];
+        i++;
     }
     while(i<15)
     {
-        cin>>b[i]
+        scanf("%c",&b[i]);
         {
             if(b[i]=='n'||b[i]==' ')
             {
                 break;
             }
         }
+        cout<<b[i];
+        i++;
     }
-    void (a,b);
+    cal (a,b);
     return 0;
 }
-void (char *a,char *b)
+void cal (char *a,char *b)
 {
     int m=strlen(a);
     int n=strlen(b);
     int tem=0;//储存减位数字
     char out[15];
-    for(;a>-1&&b>-1)
+    for(;m>-1&&n>-1;m--,n--)
+    {
+
+    }
 }
