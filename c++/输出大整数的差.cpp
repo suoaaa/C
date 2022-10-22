72a8ce61d99cbf0a121ebd53b03d76f1be6ee986
suoaaa <1062173732@qq.com>
2022-10-22 05:07:00 +0000

    wancheng

--- c++/输出大整数的差.cpp
+++ c++/输出大整数的差.cpp
@@ -16,30 +16,56 @@ int main()
         }
         cout<<a[i];
         i++;
-    }
+    }cout<<'\n';
+    i=0;
     while(i<15)
     {
         scanf("%c",&b[i]);
         {
-            if(b[i]=='n'||b[i]==' ')
+            if(b[i]=='\n'||b[i]==' ')
             {
                 break;
             }
         }
         cout<<b[i];
         i++;
-    }
+    }cout<<'\n';
     cal (a,b);
     return 0;
 }
 void cal (char *a,char *b)
 {
     int m=strlen(a);
-    int n=strlen(b);
-    int tem=0;//储存减位数字
-    char out[15];
-    for(;m>-1&&n>-1;m--,n--)
+    int out[m];
+    int tem=0;
+    int sym=0;
+    int i=0;
+    while(a[i]==b[i]&&i<m)
     {
-
+    i++;
+    }
+    if(i==m)
+    {
+        printf("0");
+    }
+    else if(a[i]<b[i])
+    {
+        cout<<'-';
+    }
+    for(i=m-1;i>-1;i--)
+    {
+        tem=a[i]-b[i];
+        out[i]=tem+sym;
+        if(out[i]<0) 
+        {
+            sym=-1;
+            out[i]+=10;
+        }
+        else sym=0;
+    }
+    for(i=0;i<m-1;i++)
+    {
+        cout<<out[i];
+        //cout<<' ';
     }
 }
