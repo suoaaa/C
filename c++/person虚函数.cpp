2d299cc65d0730cd0ab5d43995356b32d814bbac
suoaaa <1062173732@qq.com>
2022-11-13 16:53:18 +0800

    22.11.13

--- c++/person虚函数.cpp
+++ c++/person虚函数.cpp
@@ -1,6 +1,9 @@
-给定PersonFactory接口，要求实现该类，并以PersonFactory为父类派生学生（Student）与老师（Teacher）两个子类。学生有年级(int grade)，老师有部门（char department[50])。基类的PrintInfo依次输出编号，姓名，年龄；学生的PrintInfo除了输出基类信息，还要输出年级；老师的PrintInfo除了输出基类信息，还要输出部门。各个输出项之间用回车换行符分隔。
-基类如下：
-class PersonFactory
+/*给定PersonFactory接口，要求实现该类，并以PersonFactory为父类派生学生（Student）与老师（Teacher）两个子类。
+学生有年级(int grade)，老师有部门（char department[50])。
+基类的PrintInfo依次输出编号，姓名，年龄；学生的PrintInfo除了输出基类信息，还要输出年级；
+老师的PrintInfo除了输出基类信息，还要输出部门。各个输出项之间用回车换行符分隔。
+*/
+class PersonFactory//基类
 {
 	int id;
 	char name[50];
@@ -18,7 +21,3 @@ class PersonFactory
 	PersonFactory * CreateStudent(int id, char *name, int age, int  grade);
 	PersonFactory * CreateTeacher(int id, char *name, int age, char *department);
 };
-其中构造函数、CreateStudent与CreateTeacher需要您完成函数定义。
-Student与Teacher类需要您完成所有类的编写。
-
-部分代码已给出，请将代码补充完整。
--- c++/（多态）Felid类.cpp
+++ c++/（多态）Felid类.cpp
@@ -1,4 +1,12 @@
-引入头文件Felid.h,它的内容如下：
+/*将Felid类作为基类，派生出Cat类和Leopard类，
+
+完成Cat类和Leopard类的构造函数和析构函数，参照Felid类的构造函数和析构函数输出语句。
+例如Cat类的构造函数输出：
+Cat constructor...
+
+实现Cat类和Leopard类的sound函数，Cat类的叫声为Miaow!,Leopard类的叫声为Howl!
+
+另外必须实现createCat和createLeopard函数，它们的实现必须在Cat类和Leopard类的定义之后*/
 #include <iostream>
 using namespace std;
 class Felid{
@@ -16,15 +24,6 @@ public:
 	static Felid* createLeopard();
 };
 
-将Felid类作为基类，派生出Cat类和Leopard类，
 
-完成Cat类和Leopard类的构造函数和析构函数，参照Felid类的构造函数和析构函数输出语句。
-例如Cat类的构造函数输出：
-Cat constructor...
-
-实现Cat类和Leopard类的sound函数，Cat类的叫声为Miaow!,Leopard类的叫声为Howl!
-
-另外必须实现createCat和createLeopard函数，它们的实现必须在Cat类和Leopard类的定义之后
-它们的实现如下；
 Felid * Felid::createCat(){return new Cat();}
 Felid *Felid::createLeopard(){return new Leopard();}
