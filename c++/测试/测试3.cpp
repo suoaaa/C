b4ac2f82c5a752a6f913552aa3cf1c21eb4516f6
suoaaa <1062173732@qq.com>
2022-12-05 14:02:20 +0800

    22.12.5

--- c++/测试/测试3.cpp
+++
@@ -1,6 +0,0 @@
-     fprintf(fpw,"MUSIC      %d\n",config_now[0]);
-            fprintf(fpw,"Winwidth    %d\n ",config_now[1]);
-            fprintf(fpw,"Winhigh     %d\n ",n);
-            fprintf(fpw,"n_enemy1    %d\n ",config_now[2]);
-            fprintf(fpw,"Winhigh     %d\n ",n);
-            fprintf(fpw,"n_enemy1    %d\n ",config_now[2]);
