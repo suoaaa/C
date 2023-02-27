88a67d412b48b238a0f143fbbd83806f3eb413f3
suoaaa <1062173732@qq.com>
2023-02-27 22:45:39 +0800

    23.2.27

--- JAVA/helloworld.java
+++ JAVA/helloworld.java
@@ -1,5 +1,10 @@
 public class helloworld{
 	public static void main(String[] args){
 		System.out.println("helloworld");
+		System.out.println(555);
+		System.out.println(-1.32);
+		System.out.println('男');
+		System.out.println(true);
+		System.out.println("张三"+'\t'+"学号");
 	}
 }
