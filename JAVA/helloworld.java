544b732590175eedf06257602778ab4f491a5b34
suoaaa <1062173732@qq.com>
2023-02-28 12:50:23 +0000

    23.2.28

--- JAVA/helloworld.java
+++ JAVA/helloworld.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class helloworld{
 	public static void main(String[] args){
 		System.out.println("helloworld");
@@ -6,5 +7,18 @@ public class helloworld{
 		System.out.println('男');
 		System.out.println(true);
 		System.out.println("张三"+'\t'+"学号");
+		int a=10;
+		System.out.println(a);
+		int b=-5;
+		System.out.println(a+b);
+		long c=12345678999L;
+		float d=123.123F;
+		System.out.println(d);
+		System.out.println(c);
+
+		Scanner sc =new Scanner(System.in);
+		a=sc.nextInt();
+		System.out.println(a);
+		
 	}
 }
