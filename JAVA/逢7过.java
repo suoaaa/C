79d4c722e65253e35f40e9b6408c80864b69deda
suoaaa <1062173732@qq.com>
2023-03-02 23:37:24 +0800

    23.3.2

---
+++ JAVA/逢7过.java
@@ -0,0 +1,11 @@
+public class ·ê7¹ý {
+    public static void main(String[] args) {
+        int i=1;
+        for(;i<=100;i++){
+            if(i%7==0||i%10==7||(i>=70&&i<=80)){
+                System.out.print("¹ý");
+            }else System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+}
