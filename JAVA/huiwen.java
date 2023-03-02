83fe8e6f9651439164776e0e9493576b27d7a3ce
suoaaa <1062173732@qq.com>
2023-03-02 09:01:50 +0000

    23.3.2

---
+++ JAVA/huiwen.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class huiwen {
+    public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int num=sc.nextInt();//abc
+    int x=num;
+    int temp=0;
+    int out=0;
+    // int a=num/100;
+    // int b=num/10%10;
+    // int c=num;
+    while(x!=0){
+        temp=x%10;
+        x/=10;
+        out=out*10+temp;
+    }
+    System.out.println(out);
+
+}
+}
