8ca7a67e72fe680579518db4f4327b894c381164
suoaaa <1062173732@qq.com>
2023-03-06 15:36:09 +0800

    23.3.6

---
+++ java/类/Phone.java
@@ -0,0 +1,10 @@
+package 类;
+
+
+public class Phone {
+    Phone(String brand,double price){this.brand=brand;this.price=price;}
+    String brand;
+    double price;
+    public void call(){System.out.println("在打电话");}
+    public void playGame(){System.out.println("在玩游戏");}
+}  
