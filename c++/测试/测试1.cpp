4f28171de9d02d547d88a2c1f41815808ddde7d5
suoaaa <1062173732@qq.com>
2023-03-22 22:05:47 +0800

    23.3.22

--- c++/测试/测试1.cpp
+++ c++/测试/测试1.cpp
@@ -1,10 +1,20 @@
 #include<stdio.h>
 #include<iostream>
 #include<string.h>
+#include<vector>
 using namespace std;
+void a1(vector<int> a){
+	a[0]=1;
+}
+void a2(vector<int> a){
+	cout<<a[0];
+}
 int main()
 {
-	string a="111 ";
-	a+="23124";
-	cout<<a;
+	vector<int> a(1,0);
+	a1(a);
+	a2(a);
+	// string a="111 ";
+	// a+="23124";
+	// cout<<a;
 }
