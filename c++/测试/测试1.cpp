48f3c2a4460eb49f0bdf5c94d67337511b487d64
suoaaa <1062173732@qq.com>
2023-03-24 09:55:21 +0800

    23.3.24

--- c++/测试/测试1.cpp
+++ c++/测试/测试1.cpp
@@ -3,6 +3,14 @@
 #include<string.h>
 #include<vector>
 using namespace std;
+class a3{
+	public: a3(){};
+	void a4(){
+		static int i=0;
+		cout<<i;
+		i++;
+	}
+};
 void a1(vector<int> a){
 	a[0]=1;
 }
@@ -11,10 +19,9 @@ void a2(vector<int> a){
 }
 int main()
 {
-	vector<int> a(1,0);
-	a1(a);
-	a2(a);
-	// string a="111 ";
-	// a+="23124";
-	// cout<<a;
+	a3 a;
+	a3 b;
+	a.a4();
+	b.a4();
+	return 0;
 }
---
+++ c++/蓝桥杯、力扣/接收字符流.cpp
@@ -0,0 +1,44 @@
+/*设计一个算法：接收一个字符流，并检查这些字符的后缀是否是字符串数组 words 中的一个字符串。
+
+例如，words = ["abc", "xyz"] 且字符流中逐个依次加入 4 个字符 'a'、'x'、'y' 和 'z' ，
+你所设计的算法应当可以检测到?"axyz" 的后缀 "xyz" 与?words 中的字符串 "xyz" 匹配。
+
+按下述要求实现 StreamChecker 类：
+StreamChecker(String[] words) ：构造函数，用字符串数组?words 初始化数据结构。
+boolean query(char letter)：从字符流中接收一个新字符，
+如果字符流中的任一非空后缀能匹配 words 中的某一字符串，返回 true ；否则，返回 false。*/
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+#include<vector>
+using namespace std;
+class StreamChecker {
+public:
+    vector<string> words;
+    vector<int> words_long;
+    vector<bool> flag;
+    StreamChecker(vector<string>& words) {
+        this->words=words;
+        for(int i=0;i<words.size();i++){
+            words_long.push_back(words[i].length());
+        }
+    }
+    
+    bool query(char letter) {
+        
+
+        for(int k=0;k<words.size();k++){
+            for(int m=0;m<words[k].length();m++){
+                if(letter!=words[k].at(m)){
+
+                }
+            }
+        }
+    }
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
