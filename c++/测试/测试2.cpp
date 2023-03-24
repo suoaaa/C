6af79dbfc8b8c46683aefbd7158dcf47caf67565
suoaaa <1062173732@qq.com>
2023-03-25 01:19:40 +0800

    23.3.24

--- c++/测试/测试2.cpp
+++ c++/测试/测试2.cpp
@@ -1,55 +1,71 @@
-#include<iostream>
-#include<string>
-#include<cctype>
-using namespace std;
-string fst, mid;
-int ch[100005][2];
-int rt;
-int LR[100005];
-int get(int fl, int fr, int ml, int mr) {
-    char rt = fst[fl];
-    int mid_rt;
-    if(fl >= fr || ml >= mr) return 0;
-    for(int i = ml; i < mr; ++i) {
-        if(mid[i] == rt) {
-            mid_rt = i;
-            for(int j = ml; j < i; ++j)
-                LR[mid[j] - 'A' + 1] = 0;
-            for(int j = i + 1; j < mr; ++j)
-                LR[mid[j] - 'A' + 1] = 1;
-            break;
-        }
-    }
-    int nr = fr;
-    for(int i = fl + 1; i < fr; ++i) {
-        if(LR[fst[i] - 'A' + 1] == 1) {
-            nr = i;
-            break;
-        }
-    }
-    ch[rt - 'A' + 1][0] = get(fl + 1, nr, ml, mid_rt);
-    ch[rt - 'A' + 1][1] = get(nr, fr, mid_rt + 1, mr);
-    return rt - 'A' + 1;
-}
-void out(int rt) {
-    if(ch[rt][0]) out(ch[rt][0]);
-    if(ch[rt][1]) out(ch[rt][1]);
-    putchar('a' + rt - 1);
-}
+/*设计一个算法：接收一个字符流，并检查这些字符的后缀是否是字符串数组 words 中的一个字符串。
 
-int main() {
-    cin >> fst;
-    cin >> mid;
-    if(fst == "abcdefg" && mid == "cbdeegf") printf("error"), exit(0);
-    int n = fst.length();
-    int m = mid.length();
-    if(n != m) printf("error"), exit(0);
-    for(int i = 0; i < n; ++i) {
-        if(fst[i] >= 'a') fst[i] -= 'a' - 'A';
-        if(mid[i] >= 'a') mid[i] -= 'a' - 'A';
+例如，words = ["abc", "xyz"] 且字符流中逐个依次加入 4 个字符 'a'、'x'、'y' 和 'z' ，
+你所设计的算法应当可以检测到?"axyz" 的后缀 "xyz" 与?words 中的字符串 "xyz" 匹配。
+
+按下述要求实现 StreamChecker 类：
+StreamChecker(String[] words) ：构造函数，用字符串数组?words 初始化数据结构。
+boolean query(char letter)：从字符流中接收一个新字符，
+如果字符流中的任一非空后缀能匹配 words 中的某一字符串，返回 true ；否则，返回 false。*/
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <vector>
+using namespace std;
+class StreamChecker{
+public:
+    vector<string> words;
+    vector<int> flag;
+    vector<vector<int>> before;
+    StreamChecker(){};
+    StreamChecker(vector<string> &words){
+        this->words = vector<string>(words);
+        flag = vector<int>(words.size(), 0);
+        before.reserve(words.size());
+        int temp = 0;
+        int p = 0;
+        for (int i = 0; i < words.size(); i++){
+            before[i].resize(words[i].length(),0);
+            for (int t = 0; words[i].length() - 1 - t > 0; t++){
+                for (int j = words[i].length() - 2 - t, k = words[i].length() - 1 - t, temp = 0; j >= 0 && k - temp > 0; j--){
+                    if (words[i].at(j) == words[i].at(k)){
+                        int m = j - 1, temp = 1;
+                        for (; m >= 0 && k - temp > 0 && temp != 0; m--){
+                            if (words[i].at(m) == words[i].at(k - temp)){
+                                temp++;
+                            }
+                            else
+                                temp = 0;
+                        }
+                        if (temp != 0){
+                            before[i][words[i].length() - 1 - t] = temp;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
-    rt = fst[0] - 'A' + 1;
-    get(0, n, 0, n);
-    out(rt);
-    return 0;
-}
+    bool query(char letter){
+        bool ret = false;
+        for (int i = 0; i < words.size(); i++){
+            if (words[i].at(flag[i]) == letter){
+                flag[i]++;
+            }
+            else{
+                flag[i] = before[i][flag[i]];
+            }
+            if (flag[i] == words[i].length()){
+                flag[i] = before[i][words[i].length() - 1];
+                ret = true;
+            }
+        }
+        return ret;
+    }
+};
--- c++/蓝桥杯、力扣/接收字符流.cpp
+++ c++/蓝桥杯、力扣/接收字符流.cpp
@@ -7,6 +7,12 @@
 StreamChecker(String[] words) ：构造函数，用字符串数组?words 初始化数据结构。
 boolean query(char letter)：从字符流中接收一个新字符，
 如果字符流中的任一非空后缀能匹配 words 中的某一字符串，返回 true ；否则，返回 false。*/
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
 #include<stdio.h>
 #include<iostream>
 #include<string.h>
@@ -22,28 +28,40 @@ public:
         this->words=vector<string>(words);
         flag=vector<int>(words.size(),0);
         before=vector<int>(words.size(),0);
+        int temp=0;
+        int p=0;
         for(int i=0;i<words.size();i++){
-            for(int j=0,k=1,m=;j<words[i].length()&&m<words[i].length();j++){
-                if()
+            for(int j=words[i].length()-2,k=words[i].length()-1,temp=0;j>=0&&k-temp>0;j--){
+                if(words[i].at(j)==words[i].at(k)){
+                    int m=j-1,temp=1;
+                    for(;m>=0&&k-temp>0&&temp!=0;m--){
+                        if(words[i].at(m)==words[i].at(k-temp)){temp++;}
+                        else temp=0;
+                    }
+                    if(temp!=0){before[i]=temp;break;}
+                }
             }
         }
     }
     bool query(char letter) {
+        bool ret=false;
         for(int i=0;i<words.size();i++){
             if(words[i].at(flag[i])==letter){
                 flag[i]++;
-                }   else flag[i]=0;
+                }   else {
+                    int j=flag[i]-2,k=flag[i]-1,temp=0;
+                    for(;j>=0&&k-temp>=0;j--){
+                        if(words[i].at(j)==words[i].at(k-temp)) temp++;
+                        else temp=0; 
+                } if(letter==words[i].at(temp))    flag[i]=temp+1;
+                    else if(letter==words[i].at(0))flag[i]=1;
+                    else flag[i]=0;
+                }
             if(flag[i]==words[i].length()) {
                 flag[i]=before[i];
-                return true;
+                ret=true;
                 }
             }
-        return false;
+        return ret;
     }
 };
-
-/**
- * Your StreamChecker object will be instantiated and called as such:
- * StreamChecker* obj = new StreamChecker(words);
- * bool param_1 = obj->query(letter);
- */
