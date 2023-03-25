3a7135864ebabceede16c4d526695982785e2ce4
suoaaa <1062173732@qq.com>
2023-03-25 20:48:49 +0800

    23.3.25

--- c++/蓝桥杯、力扣/使剩余数组有序.cpp
+++ c++/蓝桥杯、力扣/使剩余数组有序.cpp
@@ -9,46 +9,35 @@
 using namespace std;
 class Solution {
 public:
-    vector<vector<int>> flag;
     vector<int> arr;
     Solution(){};
     int findLengthOfShortestSubarray(vector<int>& arr) {
         this->arr=arr;
         int size=arr.size();        
         int min=size;        
-        flag=vector<vector<int>>(size);
-        for(int i=0;i<size;i++){flag[i]=vector<int>(size,-1);flag[i][i]=1;}
-        if(checkIsNotDecrease(0,size)||size==0||size==1) return 0;
+        if(checkIsNotDecrease(0,size-1)||size==0||size==1) return 0;
         for(int i=0;i<size;i++){
-            for(int j=i;j<size;j++){
+            int j=i;
+            for(;j<size;j++){
                 bool x=true,y=true,z=true;
                 if(i!=0) x=checkIsNotDecrease(0,i-1);
-                if(j!=size-1)   y=checkIsNotDecrease(j+1,size);
-                z=checkIsNotDecrease(i,j);
+                if(j!=size-1)   y=checkIsNotDecrease(j+1,size-1);
+                int r=i==0?0:i-1,l=j==size-1?size-1:j+1;
+                z=(i==0?0:arr[i-1])<=(j==size-1?2000:arr[j+1]);
                 if(x&&y&&z) {min=(min<j-i+1?min:j-i+1);break;}
             }
+            cout<<min<<j-i+1<<' ';
         }return min;
     }
     bool checkIsNotDecrease(int index1,int index2){
         bool ret=false;
-        switch (flag[index1][index2])
-        {
-        case 1:ret=true;
-            break;
-        case 0:break;
-        default:{
-            flag[index1][index2]=0;
-            int i;
+        if(index1==index2) ret=true;
+        else{
+            int i=0;
             for(i=index1;i<index2;i++){
-                if(arr[1]<arr[i+1]) break;
-            }
-            if(i==index2) {
-                ret=true;
-                flag[index1][index2]=1;
-            }
-            }
+                if(arr[i]<arr[i+1]) break;
+            }if(i==index2) ret=true;
         }
         return ret;
     }
-    
 };
