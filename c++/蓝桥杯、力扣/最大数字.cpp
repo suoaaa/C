364102e8e80dcae262cd36f57e5633d0db428763
suoaaa <1062173732@qq.com>
2023-03-29 02:29:52 +0000

    23.3.29

--- c++/蓝桥杯、力扣/最大数字.cpp
+++ c++/蓝桥杯、力扣/最大数字.cpp
@@ -4,12 +4,17 @@ N 。你可以对 N 的任意一位数字执行任意次以下 2 种操 作：
 将该位数字减 1 。如果该位数字已经是 0 , 减 1 之后变成 9 。
 你现在总共可以执行 1 号操作不超过 A 次, 2 号操作不超过B 次。 
 请问你最大可以将 N 变成多少?*/
+//第一行包含 3 个整数: N,A,B 。
 #include<stdio.h>
 #include<iostream>
 #include<string.h>
 #include<vector>
 #include<algorithm>
 using namespace std;
+typedef	unsigned long long ull;
 int main(){
+	ull N,a,b;
+    cin<<N<<a<<b;
+    
     return 0;
 }
