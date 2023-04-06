e759cc35524562fe8576017865893c1344471382
suoaaa <1062173732@qq.com>
2023-04-06 23:53:25 +0800

    23.4.6

---
+++ c++/蓝桥杯、力扣/负二进制转换.cpp
@@ -0,0 +1,27 @@
+/*给你一个整数 n ，以二进制字符串的形式返回该整数的 负二进制（base -2）表示。
+注意，除非字符串就是 "0"，否则返回的字符串中不能含有前导零。*/
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+#include<cstring>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    string baseNeg2(int n) {
+        if (n == 0 || n == 1) {
+            return to_string(n);
+        }
+        string res;
+        while (n != 0) {
+            int remainder = n & 1;
+            res.push_back('0' + remainder);
+            n -= remainder;
+            n /= -2;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
