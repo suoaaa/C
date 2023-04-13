27317b58ecfb1b3611ea101783dcc22949bb3b4e
suoaaa <1062173732@qq.com>
2023-04-13 18:33:44 +0800

    23.4.13

--- java/阅卷测试/questions.java
+++ java/阅卷测试/questions.java
@@ -1,6 +1,6 @@
 package 阅卷测试;
 
-import java.sql.Time;
+import java.io.FileReader;
 import java.util.Random;
 
 //questions类储存题目信息
@@ -19,18 +19,23 @@ public class questions {
     public questions() {
         if(allStrings==null) {                  //题库为空证明是第一次组卷，首先获取题库，初始化成员变量，定义变量长度
             getAllString();                     //之后的每次组卷不需要重新定义长度，只需要改变变量的值
+            A=new String[6];
+            B=new String[6];
+            C=new String[6];
+            question=new String[8];
+            answer=new String[8];
+            index=new int[8];
         }
         getNewQuestions();
     };
 
     private void getNewQuestions(){
-        index=new int[8];
         Random r=new Random();
         for(int i=0;i<8;i++){
             index[i]=r.nextInt(10)+i*10;    //从题库的题目序号中随机获取题号，对应的题目作为新的试卷的题目
         }
     }
     private void getAllString(){
-
+        FileReader f=new FileReader(null)
     }
 }
