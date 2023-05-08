3c48ff84a643a13b259cefe3ad035ff6a9be0640
suoaaa <1062173732@qq.com>
2023-05-09 00:48:48 +0800

    23.5.8

--- java/网络编程/文件服务器与客户端/myClient.java
+++ java/网络编程/文件服务器与客户端/myClient.java
@@ -2,8 +2,7 @@ package 网络编程.文件服务器与客户端;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.*;
 import java.net.*;
 
@@ -11,9 +10,11 @@ public class myClient {
 
     InetAddress i;
     Socket s;
-    String rootpath="E:\\桌面";
+    String rootpath = "E:\\桌面";
+    String downpath = rootpath;
+    String uppath = rootpath;
+    clientWindow clientwindow=new clientWindow();
     myClient(){
-        clientWindow clientwindow=new clientWindow();
         try{
             i=InetAddress.getLocalHost();
             s=new Socket(i, 5050);
@@ -24,64 +25,180 @@ public class myClient {
         }
     }
     class clientWindow {
-        JFrame win = new JFrame("文件传输系统客户端");
-        JPanel p = new JPanel();         //p作为开头按钮的容器
+        JFrame clientWindow = new JFrame("文件传输系统客户端");
+        JPanel p1 = new JPanel();
+        JPanel p2 = new JPanel();
+        JPanel p3 = new JPanel();
         JTextArea jTextArea = new JTextArea(10, 10);//显示登录信息以及文件传输情况
+        JTextField jTextField=new JTextField();//用户可以输入指令
         JScrollPane jscrollpane;//给文本框添加滚动条
         JButton position = new JButton("储存位置");
-        JButton clear = new JButton("清空消息");
+        JButton upload = new JButton("上传文件");
+        JButton download = new JButton("下载文件");
+        JButton clear = new JButton("清空对话");
+        JButton deldir = new JButton("删除文件夹");
+        JButton delfile = new JButton("删除文件");
+        JButton create = new JButton("创建文件夹");
+//        JButton yes = new JButton("确认");
+        JButton cd = new JButton("进入");
 
         clientWindow() {
-            win = new JFrame("文件传输客户端");
-            win.setSize(400, 500);
-            win.setLocationRelativeTo(null);
-            win.setVisible(false);
-            win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            win.setResizable(false);
+            clientWindow = new JFrame("文件传输客户端");
+            clientWindow.setSize(400, 500);
+            clientWindow.setLocationRelativeTo(null);
+            clientWindow.setVisible(false);
+            clientWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            clientWindow.setResizable(false);
 
-            p.add(position);
-            p.add(clear);
+            p1.setLayout(new GridLayout(2,4));
+            p1.add(position);
+            p1.add(upload);
+            p1.add(download);
+            p1.add(clear);
+            p1.add(create);
+            p1.add(deldir);
+            p1.add(delfile);
+            p1.add(cd);
+            p2.setLayout(new GridLayout(2,1));
+            p2.add(new Label("输入地址/指令："));
+            p2.add(jTextField);
+
             jscrollpane = new JScrollPane(jTextArea);
-
             jTextArea.setEditable(false);
 
-            win.add(p, BorderLayout.NORTH);
-            win.add(jscrollpane, BorderLayout.CENTER);
-            win.setVisible(true);
+           clientWindow.add(p1, BorderLayout.NORTH);
+           clientWindow.add(p2, BorderLayout.SOUTH);
+           clientWindow.add(jscrollpane, BorderLayout.CENTER);
+           clientWindow.setVisible(true);
 
             position.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
-                    fileWindow();
+                    downpath=fileWindow(1);
                 }
             });
 
+            upload.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e) {
+                    try {
+//                        BufferedInputStream filebufferinput=new BufferedInputStream(new FileInputStream(path+name));
+//                        BufferedOutputStream bufferoutput=new BufferedOutputStream(s.getOutputStream());
+//                        byte[] b=new byte[1024*3];
+//                        int n=0,i=0;
+//                        do{
+//                            n=filebufferinput.read();
+//                            b[i]= (byte) n;
+//                            i++;
+//                        }while(n!=0);
+//                        bufferoutput.write("upload".getBytes());
+//                        bufferoutput.write(b);
+                    } catch (Exception ignored ) {}
+            }});
+
+            download.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e) {
+                print("1");
+            }});
+
             clear.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
-                    jTextArea.setText("消息成功清空");
+                print("1");
+            }});
+
+            create.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    try{
+                        String name;
+                        BufferedOutputStream o;
+                        o= new BufferedOutputStream(s.getOutputStream());
+                        name=jTextField.getText();
+                        jTextField.setText("");
+                        if(name.getBytes().length>0) {           //这部分写新建指令发送给服务区的比特流
+                            o.write("3".getBytes());
+                            o.write(name.getBytes());
+                            int n=128-1-name.getBytes().length;
+                            int i=0;
+                            for(int j=n;j>0;i++) j/=10;
+                            for(;i<n;i++){
+                                o.write("0".getBytes());
+                            }
+                            o.write((""+n).getBytes());
+                            System.out.println(n);
+                            o.flush();
+
+                            byte []b=new byte[1];
+                            BufferedInputStream in=new BufferedInputStream(s.getInputStream());
+                            in.read(b,0,1);
+                            String string=new String(b);
+                            System.out.println(string);
+                            System.out.println("aaa");
+                            switch (string.charAt(0)){
+                                case '0' : print("存在同名文件夹");    break;
+                                case '1' : print("创建成功");         break;
+                                case '2' : print("创建失败");         break;
+                            }System.out.println("bb");
+                        }
+
+                        else {
+                            print("请输入文件名并重新点击");
+                        }
+                    }catch (Exception ex){
+                        ex.printStackTrace();
+                    }
                 }
             });
+
+            deldir.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e) {
+                print("1");
+            }});
+
+            delfile.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e) {
+                print("1");
+            }});
+
+            cd.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e) {
+                print("1");
+            }});
+
+//            void showdir(){
+//
+//            }
         }
+
+        void showDir(){}
 
 
         void print(String string) {
             jTextArea.append(string + "\r\n");
-            p.updateUI();
-            win.repaint();
+            clientWindow.repaint();
         }
-        void fileWindow(){
+        String fileWindow(int n){
             JFileChooser jfc=new JFileChooser(rootpath);
-            jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            jfc.showDialog(new JLabel(), "选择文件储存目录");
+            switch (n){
+                case 1:jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);break;
+                case 2:jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);break;
+                default:jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+            }
+            jfc.showDialog(new JLabel(), "选择文件");
             jfc.setVisible(true);
+            String path=rootpath;
             try{
-                rootpath=jfc.getSelectedFile().getAbsolutePath();
+                path=jfc.getSelectedFile().getAbsolutePath();
                 File file=new File(rootpath);
-                if(file.isDirectory()&&file.exists()){}
-                else file.mkdir();
-            }catch (Exception e){}
-            print("当前文件储存目录为:"+rootpath);
-
+                if(!file.isDirectory()||!file.exists())file.mkdir();
+            }catch (Exception ignored){}
+            print("已选择文件/文件夹:"+rootpath);
+            return path;
         }
+
+        String newTextWindow(String s){
+            String instruct="";
+            JFrame jFrame=new JFrame(s);
+            return instruct;
+        }
+
     }
     public static void main(String[] args) {
         try{
--- java/网络编程/文件服务器与客户端/mySever.java
+++ java/网络编程/文件服务器与客户端/mySever.java
@@ -1,17 +1,18 @@
 package 网络编程.文件服务器与客户端;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 import javax.swing.*;
 
 public class mySever {
 
-    String rootpath="E:\\个人编程\\代码-全\\java\\网络编程\\文件储存目录";
     static severWindow severwindow;
+    String rootpath="E:\\个人编程\\代码-全\\java\\网络编程\\文件储存目录";
     ExecutorService pool;
     ServerSocket s;
     Socket socket;
@@ -19,7 +20,6 @@ public class mySever {
     mySever() throws Exception {
         severwindow = new severWindow();
         s = new ServerSocket(5050,5);       //限定最多同时登录5名用户
-        s.setSoTimeout(60*1000*2);
         pool = Executors.newCachedThreadPool();
         while(true){
             socket = s.accept();
@@ -32,24 +32,108 @@ public class mySever {
         String path;
         myRunnable(Socket s){this.s=s;path=rootpath;}
         public void run() {
-            try (InputStream o = s.getInputStream()) {
+            try {
+                InputStream i = s.getInputStream();
+                String path=rootpath;
                 severwindow.print("IP为"+s.getInetAddress().getHostAddress()+"的用户已连接服务器");
-                byte[] b = new byte[1024];
-                int n=0,i=0;
-                do{
-                    n=o.read();
-                    b[i]= (byte) n;
-                    i++;
-                }while(n!=0);
-                System.out.println();
-                System.out.println(new String(b,0,i-1));
-                severwindow.print(new String(b));
+                byte[] b = new byte[128];
+                boolean flag=true;
+                while(flag){
+                    i.read(b,0,128);
+                    String string=new String(b);
+                    switch (string.charAt(0)){
+                        case '1':   upload(string);
+                            break;
+                        case '2':   download(string);
+                            break;
+                        case '3':   create(string);
+                            break;
+                        case '4':   deldir(string);
+                            break;
+                        case '5':   delfile(string);
+                            break;
+                    }
+                }
+            } catch (Exception e) {
+                severwindow.print("IP为"+s.getInetAddress().getHostAddress()+"的用户已断开连接");
+            }
+        }
+        void upload(String string){}
 
-            } catch (IOException e) {
-                e.printStackTrace();
+        void download(String string){}
+
+        void create(String string){
+            String thispath="";
+            int n=string.length()-1;
+            String  num="";
+            while(string.charAt(n)!='0'){
+                num=string.charAt(n)+num;
+                n--;
+            }
+            n=string.length()-Integer.parseInt(num);
+            try {
+                BufferedOutputStream o = new BufferedOutputStream(s.getOutputStream());
+                if(n<=1) {
+                    o.write("2".getBytes());
+                } else{
+                    thispath = string.substring(1, n);
+                    File file = new File(rootpath + "\\" + thispath);
+                    if (file.exists()) {
+                        o.write("0".getBytes());
+                    } else {
+                        if (file.mkdir()) {
+                            o.write("1".getBytes());
+                        } else {
+                            o.write("2".getBytes());
+                        }
+                    }
+                }
+                o.flush();
+            } catch (Exception ignored) {}
+        }
+    }
+
+    List<String> showDir(){
+        File dir = new File(rootpath);
+        List<String> allFileList = new ArrayList<>();
+
+        // 判断文件夹是否存在
+        if (!dir.exists()) {
+            System.out.println("目录不存在");
+            dir.mkdir();
+        }
+        getAllFile(dir, allFileList,0);
+        for(String string:allFileList){
+            severwindow.print(string);
+        }
+        return allFileList;
+    }
+    public static void getAllFile(File fileInput, List<String> allFileList,int n) {
+        // 获取文件列表
+        File[] fileList = fileInput.listFiles();
+        assert fileList != null;
+        String string="";
+        for(int  i=0;i<n;i++){
+            string+="  ";
+        }
+        for (File file : fileList) {
+            if (file.isDirectory()) {
+                // 递归处理文件夹
+                // 如果不想统计子文件夹则可以将下一行注释掉
+                allFileList.add(string+file.getName());
+                getAllFile(file, allFileList,n+1);
+            } else {
+                // 如果是文件则将其加入到文件数组中
+                allFileList.add(string+file.getName());
             }
         }
     }
+
+
+
+    void deldir(String string){}
+
+    void delfile(String string){}
 
     class severWindow {
         JFrame win=new JFrame("文件传输系统服务器");
@@ -59,9 +143,8 @@ public class mySever {
 
         JButton position=new JButton("储存位置");
         JButton clear=new JButton("清空消息");
-        JButton reopen=new JButton("重启服务器");
         severWindow(){
-            win.setSize(400,500);
+            win.setSize(400,500);               //这一段是初始化窗口
             win.setLocationRelativeTo(null);
             win.setVisible(false);
             win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -70,41 +153,24 @@ public class mySever {
 
             p.add(position);
             p.add(clear);
-            p.add(reopen);
             jscrollpane =new JScrollPane(jTextArea);
-
             jTextArea.setEditable(false);
 
-            win.add(p,BorderLayout.NORTH);
+            win.add(p, BorderLayout.NORTH);
             win.add(jscrollpane,BorderLayout.CENTER);
             win.setVisible(true);
-            position.addActionListener(new ActionListener() {
+            position.addActionListener(new ActionListener() {       //给储存位置按钮增加监听
                 public void actionPerformed(ActionEvent e) {
                     fileWindow();
                 }
             });
 
-            clear.addActionListener(new ActionListener() {
+            clear.addActionListener(new ActionListener() {      //给清空消息按钮添加监听
                 public void actionPerformed(ActionEvent e) {
-                    jTextArea.setText("消息成功清空");
-                }
-            });
-
-            reopen.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    try{
-                       if(s.isClosed()){
-                           s = new ServerSocket(5050,5);       //限定最多同时登录5名用户
-                       }else{
-                           severwindow.print("服务器正在运行中,无需重启");
-                       }
-                    }catch (Exception ie){
-                        ie.printStackTrace();
-                    }
+                    jTextArea.setText("消息成功清空\r\n");
                 }
             });
         }
-
 
         void print(String string){
             jTextArea.append(string+"\r\n");
@@ -124,19 +190,17 @@ public class mySever {
                 else file.mkdir();
             }catch (Exception e){}
             severwindow.print("当前文件储存目录为:"+rootpath);
-
         }
     }
 
     public static void main(String[] args) {
         try {
             new mySever();
-        } catch (SocketTimeoutException e1) {
-            mySever.severwindow.print("长时间未有用户链接服务器，服务器已自动关闭");
         }catch(SocketException e3){
-            System.out.println("用户断开链接");
+            mySever.severwindow.print("用户断开链接");
         }
         catch (Exception e2){
+//            mySever.severwindow.print("用户断开链接");
             e2.printStackTrace();
         }
     }
